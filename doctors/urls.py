03fb644c53fe7d28999e4f805abc5a5daac9b35d
sravan-kumar-ta <sravan0095@gmail.com>
2022-07-01 18:39:58 +0530

    listing appointments

--- doctors/urls.py
+++ doctors/urls.py
@@ -5,5 +5,8 @@ app_name = 'doctor'
 
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
+    path('profile/patient/<int:p_id>/update/', views.UpdatePatientView.as_view(), name='doctor-update'),
     path('doctor-registration', views.add_doctor_details, name="doctor-register"),
+    path('details/update/', views.update_details, name="update-details"),
+    path('appointments/', views.AppointmentsView.as_view(), name="appointments"),
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -1,10 +1,13 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView, ListView
+
+from accounts.models import CustomUser
 from doctors.forms import DoctorDetailsForm
-
-
-# Create your views here.
-def profile(request):
-    return render(request, 'doctor/profile.html')
+from doctors.models import Doctors, Specialities
+from patients.forms import PatientUpdateForm
+from patients.models import Appointments
 
 
 def add_doctor_details(request):
@@ -13,4 +16,47 @@ def add_doctor_details(request):
         if form.is_valid():
             form.instance.details = request.user
             form.save()
-    return redirect('patient:home')
+    return redirect('doctor:profile')
+
+
+class UpdatePatientView(UpdateView):
+    model = CustomUser
+    pk_url_kwarg = 'p_id'
+    form_class = PatientUpdateForm
+    template_name = 'patient/update_patient.html'
+    success_url = reverse_lazy('doctor:profile')
+
+    def get_success_url(self):
+        messages.success(self.request, "Profile data updated successfully..!")
+        return super().get_success_url()
+
+
+def profile(request):
+    doctor = Doctors.objects.get(details_id=request.user.id)
+    specialities = Specialities.objects.all()
+    context = {
+        'doctor': doctor,
+        'specialities': specialities
+    }
+    return render(request, 'doctor/profile.html', context)
+
+
+def update_details(request):
+    special_id = request.POST.get('special')
+    charge = request.POST.get('charge')
+    special = Specialities.objects.get(id=special_id)
+
+    doctor = Doctors.objects.get(details_id=request.user.id)
+    doctor.specialized_in = special
+    doctor.charge = charge
+    doctor.save()
+    return redirect('doctor:profile')
+
+
+class AppointmentsView(ListView):
+    model = Appointments
+    context_object_name = 'appointments'
+    template_name = 'doctor/appointments.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter(doctor=self.request.user).order_by('-date_time_start')
